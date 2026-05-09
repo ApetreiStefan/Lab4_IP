c0fb10c5d9f8962599e2a69eaa0c3dcfc73867a9
SergiuZavincu <zavincusergiu44@gmail.com>
2026-05-09 10:19:13 +0300

    Implement startup event for database initialization
    Add startup event to create database tables

--- ai_service/main.py
+++ ai_service/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from ai_service.api.ai_endpoints import router as ai_endpoints_router
 
+from ai_service.db.database import engine, Base
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
 @app.get("/")
 def root():
     return {"message": "AI Service Running"}
+
 
 app.include_router(ai_endpoints_router)
